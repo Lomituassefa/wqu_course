907f2d5cd5dd08639532a575de471dc16cdf4213
Lomitu Assefa <assefa.lomitu.of@gamil.com>
2026-07-27 11:39:48 +0300

    relative path instead of absolute path

--- Project/Week_One/LocationEffect/Univariate.py
+++ Project/Week_One/LocationEffect/Univariate.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-df =(
-    pd.read_csv("/home/lomi/Desktop/wqu_courses/wqu_course/Project/mexico-real-estate-combined-clean.csv")
+import os
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, "..", "..", "mexico-real-estate-combined-clean.csv")
+df = (
+    pd.read_csv(file_path)
     .assign(price_per_m2 = lambda x: x["price_usd"]/x["area_m2"])
 )
-
 fig, ax = plt.subplots()
-
 ax.hist(df["price_per_m2"], bins = 30, edgecolor = "white")
-
 ax.set_title("Distribution of Price per Square meter")
 ax.set_xlabel("Price [USD]")
 ax.set_ylabel("Frequency")
